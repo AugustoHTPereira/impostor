791c344f0901d195174a8847a64eec28d117d2ba
Augusto Henrique Tomba Pereira <augustohtp8@gmail.com>
2026-04-08 22:49:29 -0300

    export default ap

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ export function SectionSwitcher() {
   }
 }
 
-export function App() {
+export default function App() {
   return (
     <div className="mx-auto w-full max-w-sm px-4 py-6">
       <ImpostorProvider>
